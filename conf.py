72007528bdb5282b3487979ea3674a6c68bcfe93
RishiMahamuni <rjmahamuni80@gmail.com>
2024-05-27 22:23:56 +0530

    conf updated

--- conf.py
+++ conf.py
@@ -21,3 +21,26 @@ html_css_files = [
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'my_style.MyStyle'
+
+# conf.py
+
+# Import the Furo theme
+html_theme = "furo"
+
+# Specify Pygments styles for code highlighting
+pygments_style = "sphinx"  # for light mode
+pygments_dark_style = "monokai"  # for dark mode
+
+
+html_theme_options = {
+    "light_css_variables": {
+        "color-brand-primary": "#7C4DFF",
+        "color-brand-content": "#7C4DFF",
+    },
+    "dark_css_variables": {
+        "color-brand-primary": "#FF79C6",
+        "color-brand-content": "#FF79C6",
+    }
+}
+
+
