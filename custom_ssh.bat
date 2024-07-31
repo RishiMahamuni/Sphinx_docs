b4533acfb77cdb4fab6e275c0c78b822500704d8
RishiMahamuni <rjmahamuni80@gmail.com>
2024-07-31 12:42:31 +0530

    news scripts

---
+++ custom_ssh.bat
@@ -0,0 +1,4 @@
+@echo off
+rem Custom SSH script using Tectia SSH
+set SSH2_PATH="C:\Program Files\Tectia\SSH Client\ssh2.exe"
+%SSH2_PATH% %*
---
+++ custtom_ssh.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Custom SSH script using Tectia SSH
+SSH2_PATH="/usr/local/bin/ssh2"
+exec "$SSH2_PATH" "$@"
